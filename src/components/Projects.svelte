e51274bb757f2784f58b3bc397adea186ed7a538
Ross MacDonald <rmac2289@gmail.com>
2021-01-03 05:34:26 -0800

    adding projects

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -1,10 +1,21 @@
 <script>
     import Layout from "./Layout.svelte";
+    let meal = "images/meal.png";
+    let fyp = "images/fyp.png";
+    let fypmobile = "images/fypmobilemap.jpg";
+    let hearsay = "images/hearsay.png";
+    let safety = "images/safety.PNG";
+    import { Router, Route } from "svelte-routing";
+    console.dir(Router);
 </script>
 
 <style>
     .header {
         color: white;
+    }
+    .grid {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
     }
 </style>
 
@@ -13,4 +24,20 @@
 </svelte:head>
 <Layout>
     <h1 class="header">Projects</h1>
+    <div class="grid">
+        <div id="meal">
+            <img width="400" height="450" src={meal} alt="meal" />
+        </div>
+        <div id="fyp"><img width="500" height="325" src={fyp} alt="fyp" /></div>
+        <div id="hearsay">
+            <img width="500" height="325" src={hearsay} alt="hearsay" />
+        </div>
+        <div id="fypmobile">
+            <img width="175" height="325" src={fypmobile} alt="fypmobile" />
+        </div>
+
+        <div id="safety">
+            <img width="175" height="325" src={safety} alt="safety" />
+        </div>
+    </div>
 </Layout>
