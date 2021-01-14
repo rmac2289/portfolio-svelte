f46eb4cce3f48c5e111525a364b78e3bbadd447f
Ross MacDonald <rmac2289@gmail.com>
2021-01-14 05:17:11 -0800

    fixed spacing issue on projects page

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -1,76 +1,36 @@
 <script>
-    import Layout from "./utils/Layout.svelte";
-    import SafetyBlanket from "./projects/SafetyBlanket.svelte";
-    import Hearsay from "./projects/Hearsay.svelte";
-    import FypMobile from "./projects/FypMobile.svelte";
-    import Fyp from "./projects/Fyp.svelte";
-    import Portfolio from "./projects/Portfolio.svelte";
-    import MealGenerator from "./projects/MealGenerator.svelte";
-    import { fly } from "svelte/transition";
+  import Layout from "./utils/Layout.svelte";
+  import SafetyBlanket from "./projects/SafetyBlanket.svelte";
+  import Hearsay from "./projects/Hearsay.svelte";
+  import FypMobile from "./projects/FypMobile.svelte";
+  import Fyp from "./projects/Fyp.svelte";
+  import Portfolio from "./projects/Portfolio.svelte";
+  import MealGenerator from "./projects/MealGenerator.svelte";
+  import { fly } from "svelte/transition";
 </script>
 
-<style>
-    .grid {
-        display: grid;
-        grid-template-columns: 1fr;
-        gap: 30px;
-        width: 95%;
-        padding: 10px;
-    }
-</style>
-
 <svelte:head>
-    <title>Projects</title>
+  <title>Projects</title>
 </svelte:head>
 <Layout>
-    <div class="grid" transition:fly={{ duration: 1000, y: 500 }}>
-        <SafetyBlanket />
-        <Fyp />
-        <Hearsay />
-        <Portfolio />
-        <FypMobile />
-        <MealGenerator />
-    </div>
+  <div class="grid" transition:fly={{ duration: 1000, y: 500 }}>
+    <SafetyBlanket />
+    <Fyp />
+    <Hearsay />
+    <Portfolio />
+    <FypMobile />
+    <MealGenerator />
+  </div>
 </Layout>
 
-<!-- <div id="meal">
-    <Image
-        width="400"
-        height="450"
-        cloud_name="de36vblcl"
-        public_id="meal"
-        alt="meal" />
-</div>
-<div id="fyp">
-    <Image
-        width="500"
-        height="325"
-        cloud_name="de36vblcl"
-        public_id="fyphome"
-        alt="fyp" />
-</div>
-<div id="hearsay">
-    <Image
-        width="500"
-        height="325"
-        cloud_name="de36vblcl"
-        public_id="hearsayhome"
-        alt="hearsay" />
-</div>
-<div id="fypmobile">
-    <Image
-        width="175"
-        height="325"
-        cloud_name="de36vblcl"
-        public_id="fypmobile"
-        alt="fypmobile" />
-</div>
-
-<div id="safety">
-    <Image
-        cloud_name="de36vblcl"
-        width="175"
-        height="325"
-        public_id="safetyhome"
-        alt="safety" />
-</div> -->
+<style>
+  .grid {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 30px;
+    width: 95%;
+    padding: 10px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+</style>
--- src/components/Resume.svelte
+++ src/components/Resume.svelte
@@ -1,35 +1,35 @@
 <script>
-    import Layout from "./utils/Layout.svelte";
+  import Layout from "./utils/Layout.svelte";
 </script>
 
-<style>
-    .container {
-        width: 100%;
-        height: 100%;
-        margin-left: auto;
-        margin-right: auto;
-        text-align: center;
-    }
-    .iframe {
-        width: 350px;
-        height: 520px;
-        opacity: 0.75;
-        margin-left: auto;
-        margin-right: auto;
-    }
-    @media only screen and (min-width: 720px) {
-        .iframe {
-            min-height: 1200px;
-            min-width: 800px;
-        }
-    }
-</style>
-
 <svelte:head>
-    <title>Resume</title>
+  <title>Resume</title>
 </svelte:head>
 <Layout>
-    <div class="container">
-        <iframe class="iframe" src="images/resume.pdf" title="resume" />
-    </div>
+  <div class="container">
+    <iframe class="iframe" src="images/resume.pdf" title="resume" />
+  </div>
 </Layout>
+
+<style>
+  .container {
+    width: 100%;
+    height: 100%;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+  }
+  .iframe {
+    width: 350px;
+    height: 520px;
+    opacity: 0.85;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  @media only screen and (min-width: 720px) {
+    .iframe {
+      min-height: 1200px;
+      min-width: 800px;
+    }
+  }
+</style>
